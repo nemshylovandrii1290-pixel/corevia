84c300986aa8e3daaf451c9a572929de3e5f60ba
Andrii Nemshylov <nemshylovandrii1290@gmail.com>
2026-02-26 18:23:53 +0100

    Initial commit - Corevia foundation

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  root: './src',
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5174,
+    open: true,
+  },
+});
