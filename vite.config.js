2463e3a9847c163c9e55e0610dff76cf2076c1c3
Andrii Nemshylov <nemshylovandrii1290@gmail.com>
2026-02-27 02:02:40 +0100

    fix vite root config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  root: './src',
   plugins: [react()],
   server: {
     host: true,
